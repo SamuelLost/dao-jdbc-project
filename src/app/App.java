a66d96a1bad3f0835807a2cea37b6ed4b91ed245
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2023-01-24 19:56:48 -0300

    Implementação do findAll

--- src/app/App.java
+++ src/app/App.java
@@ -17,6 +17,9 @@ public class App {
         System.out.println("\n=== TEST 2: seller findByDepartment ===");
         Department department = new Department(2, null);
         sellerDao.findByDepartment(department).forEach(System.out::println);
+
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        sellerDao.findAll().forEach(System.out::println);
         
     }
 }
