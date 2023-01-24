66388cdd0e26362d654aff15ebd9191d640b8017
Samuel Henrique <samuelhenriqec@alu.ufc.br>
2023-01-24 18:23:46 -0300

    Implementação do findById

--- src/app/App.java
+++ src/app/App.java
@@ -1,20 +1,15 @@
 package app;
 
-import java.util.Date;
-
 import model.dao.DAOFactory;
 import model.dao.SellerDAO;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Department obj = new Department(1, "Books");
-        
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDAO sellerDao = DAOFactory.createSellerDAO();
+
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
--- src/model/dao/DAOFactory.java
+++ src/model/dao/DAOFactory.java
@@ -1,12 +1,13 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDAOJDBC;
 
 public class DAOFactory {
     
     public static SellerDAO createSellerDAO() {
-        return new SellerDAOJDBC();
+        return new SellerDAOJDBC(DB.getConnection());
     }
 
-    
+
 }
